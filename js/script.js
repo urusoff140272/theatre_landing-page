80bc9eb31e54ca96de03f4fd0caa305859b38bd6
urusoff140272 <107813230+urusoff140272@users.noreply.github.com>
2024-05-08 07:03:56 +0300

    feat: add html, css, swiper sections header and top

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-"use strict"
+const swiperTop = new Swiper('.top__slider', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    effect: 'cards',
+    cardsEffect: {
+        perSlideOffset: 10,
+        perSlideRotate: 0,
+        slideShadows:false,
+        rotate:false,
+    },
+});
